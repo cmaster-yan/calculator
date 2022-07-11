86f2247c80646445009a16c944ed0f4538c06f84
cmaster-yanx <yancruz9611@gmail.com>
2022-07-11 15:24:36 -0700

    Add design and partially functioning logic The calculator has its basic design, it displays numbers, can do the 4 basic operations but only the first number can be more than one digit, this is an error to fix. = signs shows the result as well

---
+++ calc.js
@@ -0,0 +1,317 @@
+const display = document.getElementById('screen');
+const buttons = document.getElementById('buttons').querySelectorAll('button');
+buttons.forEach(item => item.classList.add('btn-' + item.innerText));
+buttons.forEach(item => item.addEventListener('click', () => {
+    pressButton(item);
+}))
+let operand1 = '';
+let operand2 = '';
+let operator = '';
+let pressed = 0;
+let lastPressed = '';
+
+function pressButton(button){
+    function isNumber(){
+        let numbers = ['btn-0', 'btn-1','btn-2','btn-3','btn-4','btn-5',
+                        'btn-6','btn-7','btn-8','btn-9','btn-0'];
+        if (numbers.indexOf(button.getAttribute('class')) != -1) {
+            return true;
+        }
+    }
+    if (isNumber()){
+        switch(button.getAttribute('class')) {
+            case 'btn-0':
+                if(pressed == 1){
+                    display.innerText = 0;
+                }
+                if(display.innerText == '0'){
+                    display.innerText = '';
+                }
+                display.innerText += '0';
+                lastPressed = 'num';
+                break;
+            case 'btn-1':
+                if(pressed == 1){
+                    display.innerText = 0;
+                }
+                if(display.innerText == '0'){
+                    display.innerText = '';
+                }
+                display.innerText += '1';
+                lastPressed = 'num';
+                break;
+            case 'btn-2':
+                if(pressed == 1){
+                    display.innerText = 0;
+                }
+                if(display.innerText == '0'){
+                    display.innerText = '';
+                }
+                display.innerText += '2';
+                lastPressed = 'num';
+                break;
+            case 'btn-3':
+                if(pressed == 1){
+                    display.innerText = 0;
+                }
+                if(display.innerText == '0'){
+                    display.innerText = '';
+                }
+                display.innerText += 3;
+                lastPressed = 'num';
+                break;
+            case 'btn-4':
+                if(pressed == 1){
+                    display.innerText = 0;
+                }
+                if(display.innerText == '0'){
+                    display.innerText = '';
+                }
+                display.innerText += '4';
+                lastPressed = 'num';
+                break;
+            case 'btn-5':
+                if(pressed == 1){
+                    display.innerText = 0;
+                }
+                if(display.innerText == '0'){
+                    display.innerText = '';
+                }
+                display.innerText += '5';
+                lastPressed = 'num';
+                break;
+            case 'btn-6':
+                if(pressed == 1){
+                    display.innerText = 0;
+                }
+                if(display.innerText == '0'){
+                    display.innerText = '';
+                }
+                display.innerText += '6';
+                lastPressed = 'num';
+                break;
+            case 'btn-7':
+                if(pressed == 1){
+                    display.innerText = 0;
+                }
+                if(display.innerText == '0'){
+                    display.innerText = '';
+                }
+                display.innerText += '7';
+                lastPressed = 'num';
+                break;
+            case 'btn-8':
+                if(pressed == 1){
+                    display.innerText = 0;
+                }
+                if(display.innerText == '0'){
+                    display.innerText = '';
+                }
+                display.innerText += '8';
+                lastPressed = 'num';
+                break;
+            case 'btn-9':
+                if(pressed == 1){
+                    display.innerText = 0;
+                }
+                if(display.innerText == '0'){
+                    display.innerText = '';
+                }
+                display.innerText += '9';
+                lastPressed = 'num';
+                break;
+        }
+    }
+    else {
+        switch(button.getAttribute('class')) {
+            case 'btn-AC':
+                operand1 = '';
+                operand2 = '';
+                operator = '';
+                display.innerText = '0';
+                pressed = 0;
+                break;
+            case 'btn-+/-':
+                operand1 += button.innerText;
+                display.innerText = operand1;
+                break;
+            case 'btn-%':
+                operand1 += button.innerText;
+                display.innerText = operand1;
+                break;
+            case 'btn-/':
+                if (lastPressed == 'op'){
+                    operator = button.innerText;
+                    return;
+                }
+                lastPressed = 'op'
+                if(operator != '' && operator != button.innerText){
+                    operand2 = display.innerText;
+                    operand1 = operate(operand1, operator, operand2);
+                    display.innerText = operand1
+                    operator = button.innerText;
+                    return;
+                }
+                pressed = 1;
+                if(operand1 == ''){
+                    operator = button.innerText;
+                    operand1 = display.innerText;
+                    operand1 = operate(operand1, operator, 1);
+                }
+                else {
+                    operand2 = display.innerText
+                    operand1 = operate(operand1, operator, operand2);
+                    display.innerText = operand1;
+                }
+                break;
+            case 'btn-*':
+                if (lastPressed == 'op'){
+                    operator = button.innerText;
+                    return;
+                }
+                lastPressed = 'op'
+                if(operator != '' && operator != button.innerText){
+                    operand2 = display.innerText;
+                    operand1 = operate(operand1, operator, operand2);
+                    display.innerText = operand1
+                    operator = button.innerText;
+                    return;
+                }
+                pressed = 1;
+                if(operand1 == ''){
+                    operator = button.innerText;
+                    operand1 = display.innerText;
+                    operand1 = operate(operand1, operator, 1);
+                }
+                else {
+                    operand2 = display.innerText
+                    operand1 = operate(operand1, operator, operand2);
+                    display.innerText = operand1;
+                }
+                break;
+            case 'btn--':
+                if (lastPressed == 'op'){
+                    operator = button.innerText;
+                    return;
+                }
+                lastPressed = 'op'
+                if(operator != '' && operator != button.innerText){
+                    operand2 = display.innerText;
+                    operand1 = operate(operand1, operator, operand2);
+                    display.innerText = operand1
+                    operator = button.innerText;
+                    return;
+                }
+                pressed = 1;
+                if(operand1 == ''){
+                    operator = button.innerText;
+                    operand1 = display.innerText;
+                    operand1 = operate(operand1, operator, 0);
+                }
+                else {
+                    operand2 = display.innerText
+                    operand1 = operate(operand1, operator, operand2);
+                    display.innerText = operand1;
+                }
+                break;
+            case 'btn-+':
+                if (lastPressed == 'op'){
+                    operator = button.innerText;
+                    return;
+                }
+                pressed = 1;
+                lastPressed = 'op'
+                if(operator != '' && operator != button.innerText){
+                    operand2 = display.innerText;
+                    operand1 = operate(operand1, operator, operand2);
+                    display.innerText = operand1
+                    operator = button.innerText;
+                    return;
+                }
+                if(operand1 == ''){
+                    operator = button.innerText;
+                    operand1 = display.innerText;
+                    operand1 = operate(operand1, operator, 0);
+                }
+                else {
+                    operand2 = display.innerText
+                    operand1 = operate(operand1, operator, operand2);
+                    display.innerText = operand1;
+                }
+                break;
+            case 'btn-=':
+                lastPressed = 'op';
+                operand2 = display.innerText;
+                display.innerText = operate(operand1, operator, operand2);
+                operand1 = display.innerText;
+                operand2 = ''
+                break;
+            case 'btn-.':
+                operand1 += button.innerText;
+                display.innerText = operand1;
+                break;
+        }
+    }
+}
+
+function operate(a,operator,b){
+    if(operator == '+'){
+        return add(+a, +b);
+    }
+    else if(operator == '-'){
+        return substract(a,b);
+    }
+    else if(operator == '*'){
+        return multiply(a,b);
+    }
+    else if(operator == '/'){
+        return divide(a,b)
+    }
+    function add(a,b) {
+        return a+b;
+    }
+    
+    function substract(a,b) {
+        return a-b;
+    }
+    
+    function multiply(a,b) {
+        return a*b;
+    }
+    
+    function divide(a,b) {
+        if(b==0){
+            return 'ERROR';
+        }
+        else {
+            return a/b;
+        }
+    }
+}
+
+//va despues de isnumber()
+/* if (isNumber()) {
+    if (operator == ''){
+    operand1 += button.innerText;
+    display.innerText = operand1;
+    }
+else {
+    operand2 += button.innerText;
+    display.innerText = operand2;
+}
+}
+if (!isNumber()){
+    if(button.innerText == 'AC'){
+        display.innerText = '0';
+    }
+    if(button.innerText == '=') {
+        display.innerText = operate(operand1, operator, operand2);
+
+    }
+    if(operand1 != '' && operator != '' && operand2 !== ''){
+        display.innerText = operate(operand1, operator, operand2);nuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu
+    }
+    else{
+        operator = button.innerText;
+    }
+} */
